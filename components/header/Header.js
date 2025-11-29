b1aa57ee6f87f1ef6897e4eecd6f5d8503185726
Abdulmanan-Rahimi <abdulmananrahimi5565@gmail.com>
2025-11-29 04:30:16 +0430

    I completed the about page in my project

---
+++ components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import MainMenu from "./mainMenu/MainMenu";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar />
+      <MainMenu />
+    </>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,5 @@ import MainMenu from "@/components/header/mainMenu/MainMenu";
 import Navbar from "@/components/header/navbar/Navbar";
 
 export default function Home() {
-  return (
-    <>
-     
-      <Navbar />
-      <MainMenu />
-      
-    </>
-  );
+  return <></>;
 }
