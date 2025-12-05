9f4f07199bb69dd79338a643e80f24bb73316500
Abdulmanan-Rahimi <abdulmananrahimi5565@gmail.com>
2025-12-05 22:54:35 +0430

    I maked one productCard in my project

--- components/productCard/ProductCard.js
+++ components/productCard/ProductCard.js
@@ -2,7 +2,23 @@ import React from "react";
 import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
-  return <div>ProductCard</div>;
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardHeader}>
+        <img src="https://dl.next1code.ir/images/nextjs/chapter04/laptop01.jpg" />
+      </div>
+      <div className={styles.cardMiddle}>
+        <p>
+          لپ تاپ لنوو 15.6 اینچی مدل ThinkBook 15 پردازنده Core i3 1115G4 رم 8GB
+          حافظه 256GB SSD
+        </p>
+      </div>
+      <div className={styles.cardFooter}>
+        <button>افزودن به سبد خرید</button>
+        <p>24,900,000</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import MainMenu from "@/components/header/mainMenu/MainMenu";
 import Navbar from "@/components/header/navbar/Navbar";
+import ProductCard from "@/components/productCard/ProductCard";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div style={{margin : '50px' , textAlign : 'center'}}>
+      <ProductCard />
+    </div>
+  )
 }
