b25f974ec9bbb6af2a0d254ebff8bd49663f484e
Abdulmanan-Rahimi <abdulmananrahimi5565@gmail.com>
2025-12-07 03:56:27 +0430

    I completed the laptops section of home page in my project

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import MainMenu from "@/components/header/mainMenu/MainMenu";
 import Navbar from "@/components/header/navbar/Navbar";
 import ProductCard from "@/components/productCard/ProductCard";
+import db from "../data/db.json";
 
 export default function Home() {
   return (
-    <div style={{margin : '50px' , textAlign : 'center'}}>
-      <ProductCard />
+    <div className="container">
+      <div className="row">
+        {db.laptops.slice(0, 4).map((laptop) => (
+          <div key={laptop.id} className="col">
+            <ProductCard {...laptop} />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
