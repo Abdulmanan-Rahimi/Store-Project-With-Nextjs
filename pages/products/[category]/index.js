ecbc07df52b7788cedf25fe2ae8c9ddbb952fcb5
Abdulmanan-Rahimi <abdulmananrahimi5565@gmail.com>
2025-12-07 06:25:51 +0430

    I completed the cameras section of home page in my project

--- pages/products/[category]/index.js
+++ pages/products/[category]/index.js
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router";
 import React from "react";
+import db from "@/data/db.json";
 
 const ProductsByCategory = () => {
   const { category } = useRouter().query;
+  console.log(db[category]);
 
   return <div>ProductsByCategory - {category}</div>;
 };
